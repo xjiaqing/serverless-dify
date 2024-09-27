400382d53fd7a0c49cb55002ae110875175bc10a
xjiaqing <ballieasor@outlook.com>
2024-09-27 19:43:14 +0800

    init cdk project

---
+++ test/serverless-dify.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ServerlessDify from '../lib/serverless-dify-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/serverless-dify-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ServerlessDify.ServerlessDifyStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
