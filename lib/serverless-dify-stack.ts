074aa7b25fdd5b68d02bb03313e87817fe0bbf25
xjiaqing <ballieasor@outlook.com>
2024-09-27 19:43:56 +0800

    update stack

---
+++ lib/serverless-dify-stack.ts
@@ -0,0 +1,14 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { MetadataStorageStack } from '../src/metadata-storage';
+import { VpcStack } from '../src/vpc-stack';
+
+export class ServerlessDifyStack extends cdk.Stack {
+	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+		super(scope, id, props);
+
+		const vpc = new VpcStack(this, 'dify-vpc')
+
+		new MetadataStorageStack(this, 'dify-metadata-storage', { vpc: vpc.vpc })
+	}
+}
