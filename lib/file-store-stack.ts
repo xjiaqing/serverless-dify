dd6e615c95c20af800d78d9f52d9cd0d5fc21992
xjiaqing <ballieasor@outlook.com>
2024-12-02 16:07:24 +0800

    update bucket policy and add lifecycle rule for s3 bucket.

--- lib/file-store-stack.ts
+++ lib/file-store-stack.ts
@@ -1,4 +1,4 @@
-import { RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
+import { Duration, RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
 import { Bucket } from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
 import { DifyFileStoreProps } from "./task-definitions/props";
@@ -10,9 +10,17 @@ export class FileStoreStack extends Stack {
     constructor(scope: Construct, id: string, props: StackProps) {
         super(scope, id, props)
 
-        this.bucket = new Bucket(this, 'ObjectStorage', {
-            removalPolicy: RemovalPolicy.DESTROY
+
+        this.bucket = new Bucket(this, 'ServerlessDifyObjectFileStore', {
+            removalPolicy: RemovalPolicy.DESTROY,
+            versioned: true,
         });
+
+        this.bucket.addLifecycleRule({
+            abortIncompleteMultipartUploadAfter: Duration.days(1),
+            noncurrentVersionExpiration: Duration.days(7),
+            noncurrentVersionsToRetain: 2,
+        })
     }
 
     public exportProps(): DifyFileStoreProps {
